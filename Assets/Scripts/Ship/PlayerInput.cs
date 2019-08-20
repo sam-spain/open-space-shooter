cdfd3734a11292c941dc216f2a647a92e8874137
RYGEL\Sam Spain <sam@samspain.com>
2019-08-20 20:02:26 +0100

    Player movement that's limited to screen bounds.
    Took a while because the scene is facing performance issues. May be related to 2019.3 if so, may have to revert to earlier version and hope Linux works fine.

---
+++ Assets/Scripts/Ship/PlayerInput.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ShipMotor))]
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private ShipMotor shipMotor;
+
+    private void Update()
+    {
+        movement();
+    }
+
+    private void movement()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        Vector3 screenSpace = new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z);
+        Vector2 screenBounds = Camera.main.ScreenToWorldPoint(screenSpace);
+
+        if (yInput > 0 && transform.position.y > screenBounds.y - 1)
+        {
+            yInput = 0;
+        }
+        else if (yInput < 0 && transform.position.y < -screenBounds.y + 1)
+        {
+            yInput = 0;
+        }
+
+        if (xInput > 0 && transform.position.x > screenBounds.x - 1)
+        {
+            xInput = 0;
+        }
+        else if (xInput < 0 && transform.position.x < -screenBounds.x + 1)
+        {
+            xInput = 0;
+        }
+
+        shipMotor.setInput(xInput, yInput);
+    }
+}
