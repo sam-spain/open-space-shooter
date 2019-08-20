372d6522a179caba1ec0850d1015189f807858fa
Sam Spain <sam@samspain.com>
2019-08-20 20:27:48 +0100

    Player controls (#29)
    * Add ignoring meta files
    
    Why the hell was this not there already. The other ones wouldn't work on a Unity project. >:(
    
    * Player movement that's limited to screen bounds.
    
    Took a while because the scene is facing performance issues. May be related to 2019.3 if so, may have to revert to earlier version and hope Linux works fine.
    
    * Addressed lint issues
    
    Extracted bool checks to explain values
    
    * Added TextMeshPro

---
+++ Assets/Scripts/Ship/PlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ShipMotor))]
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private ShipMotor shipMotor;
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        Vector3 screenSpace = new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z);
+        Vector2 screenBounds = Camera.main.ScreenToWorldPoint(screenSpace);
+
+        int screenPadding = 1;
+
+        bool tooFarUp = yInput > 0 && transform.position.y > screenBounds.y - screenPadding;
+        bool tooFarDown = yInput < 0 && transform.position.y < -screenBounds.y + screenPadding;
+        if (tooFarUp || tooFarDown)
+        {
+            yInput = 0;
+        }
+
+        bool tooFarRight = xInput > 0 && transform.position.x > screenBounds.x - screenPadding;
+        bool rooFarLeft = xInput < 0 && transform.position.x < -screenBounds.x + screenPadding;
+        if (tooFarRight || rooFarLeft)
+        {
+            xInput = 0;
+        }
+
+        shipMotor.SetInput(xInput, yInput);
+    }
+}
---
+++ Assets/Scripts/Ship/ShipMotor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using static UnityEngine.Time;
+
+public class ShipMotor : MonoBehaviour
+{
+    private float xInput;
+    private float yInput;
+
+    [SerializeField]
+    private float baseShipSpeed = 1f;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * xInput * baseShipSpeed * deltaTime);
+        transform.Translate(Vector3.up * yInput * baseShipSpeed * deltaTime);
+    }
+
+    internal void SetInput(float xInput, float yInput)
+    {
+        this.xInput = xInput;
+        this.yInput = yInput;
+    }
+}
