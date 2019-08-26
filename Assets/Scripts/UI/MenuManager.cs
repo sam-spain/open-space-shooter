de6621bcd91e4f6ce81b844cf364e5a830ce853a
Sam Spain <sam@samspain.com>
2019-08-26 13:03:23 +0100

    Menu can now be toggle (#33)
    For later use. Will need to be more dynamic in future.

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenu;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)) 
+        {
+            TogglePause();
+        }
+    }
+    public void TogglePause() 
+    {
+        if(pauseMenu.activeSelf) 
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        } else 
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+    }
+}
