aea953001ea83f73adc16f29ffd6d77560d3b4d2
Sam <sam@samspain.com>
2020-05-01 22:08:21 +0100

    Clamp player position in own class

--- Assets/Scripts/SpaceEntity/MotorService.cs
+++ Assets/Scripts/SpaceEntity/MotorService.cs
@@ -19,18 +19,6 @@ namespace SpaceObjects
             xInput = 0;
             yInput = 0;
         }
-        private void LateUpdate()
-        {
-            // TODO: Use ClampPosition in player specific component
-        }
-
-        private void ClampPosition()
-        {
-            Vector3 position = Camera.main.WorldToViewportPoint(transform.position);
-            position.x = Mathf.Clamp(position.x, 0.05F, 0.95F);
-            position.y = Mathf.Clamp(position.y, 0.1F, 0.9F);
-            transform.position = Camera.main.ViewportToWorldPoint(position);
-        }
 
         internal void SetInput(float xInput, float yInput)
         {
