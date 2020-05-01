3cb97cb6c969d709c588c219834ee0cc72aa3c66
Sam <sam@samspain.com>
2020-05-01 20:56:38 +0100

    Make initial enemy spawner

---
+++ Assets/Scripts/SpaceEntity/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace SpaceObjects
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject spawnPrefab;
+
+        [SerializeField]
+        private SpawnPoint[] spawnPoints;
+
+        [SerializeField]
+        private float spawnRate = 1F;
+        private float spawnCounter = 0F;
+        private void Start()
+        {
+            spawnPoints = GetComponentsInChildren<SpawnPoint>();
+        }
+
+        private void Update()
+        {
+            spawnCounter += Time.deltaTime;
+            if (spawnCounter > spawnRate)
+            {
+                Spawn();
+                spawnCounter = 0;
+            }
+        }
+
+        internal void Spawn()
+        {
+            Instantiate(spawnPrefab,
+                spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position,
+                Quaternion.identity);
+        }
+    }
+}
